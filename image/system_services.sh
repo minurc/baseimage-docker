5d356a962633944a54707081783c73b3014ac5f5
Milan Uremovic <milan.uremovic@gmail.com>
2015-03-26 18:17:38 +0100

    Change from syslog-ng to rsyslog

--- image/system_services.sh
+++ image/system_services.sh
@@ -20,16 +20,22 @@ ln -s /etc/container_environment.sh /etc/profile.d/
 $minimal_apt_get_install runit
 
 ## Install a syslog daemon.
-$minimal_apt_get_install syslog-ng-core
-mkdir /etc/service/syslog-ng
-cp /build/runit/syslog-ng /etc/service/syslog-ng/run
-mkdir -p /var/lib/syslog-ng
-cp /build/config/syslog_ng_default /etc/default/syslog-ng
+# $minimal_apt_get_install syslog-ng-core
+mkdir /etc/service/rsyslog
+cp /build/runit/rsyslog /etc/service/rsyslog/run
+# mkdir -p /var/lib/syslog-ng
+cp /build/config/rsyslog_default /etc/default/rsyslog
 touch /var/log/syslog
 chmod u=rw,g=r,o= /var/log/syslog
 # Replace the system() source because inside Docker we
 # can't access /proc/kmsg.
-sed -i -E 's/^(\s*)system\(\);/\1unix-stream("\/dev\/log");/' /etc/syslog-ng/syslog-ng.conf
+# sed -i -E 's/^(\s*)system\(\);/\1unix-stream("\/dev\/log");/' /etc/syslog-ng/syslog-ng.conf
+rm -f /etc/rsyslog.d/*
+cp /build/config/rsyslog_conf /etc/rsyslog.conf
+chmod u=rw,g=r,o=r /etc/rsyslog.conf
+cp /build/config/rsyslog_conf_70 /etc/rsyslog.d/70-remote.conf
+chmod u=rw,g=r,o=r /etc/rsyslog.d/*
+
 
 ## Install syslog to "docker logs" forwarder.
 mkdir /etc/service/syslog-forwarder
